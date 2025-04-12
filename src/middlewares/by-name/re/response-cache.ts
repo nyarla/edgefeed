f197b06a7535f087424e9635af73389b488ddb64
nyarla <nyarla@kalaclista.com>
2025-04-12 12:02:45 +0900

    docs(middlewares/response-cache): rewrite code with the better documentation

--- src/middlewares/by-name/re/response-cache.ts
+++ src/middlewares/by-name/re/response-cache.ts
@@ -1,25 +1,29 @@
-import type { Context, Next } from "hono";
+import type { Context, Next, MiddlewareHandler } from "hono";
 import type { ICacheOpener } from "../../../interfaces/cache";
 
 /**
  * The cache middleware for anything.
  *
  * This middleware uses cache namespace from Hono's `c.req.path`.
- * 
+ *
+ * @param open - the cache opener with `ICache` interface.
+ * @returns - the Hono's middleware handler for the cachting.
+ *
  * @example
-* ```ts
-* import {Hono} from 'hono';
-* import {middleware as cache} from '@/middleware/by-name/re/response-cache';
- 
-* const app = new Hono();
-* app.get('*', cache(caches.open));
-* app.get('/', (c) => c.text('hi!'));
-*
-  export default app;
-* ```
+ * ```ts
+ * import {Hono} from 'hono';
+ * import {middleware as cache} from '@/middleware/by-name/re/response-cache';
+ *
+ * const app = new Hono();
+ * app.get('*', cache(caches.open));
+ * app.get('/', (c) => c.text('hi!'));
+ *
+ * export default app;
+ * ```
  */
 export const middleware =
-  (open: ICacheOpener) => async (c: Context, next: Next) => {
+  (open: ICacheOpener): MiddlewareHandler =>
+  async (c: Context, next: Next) => {
     const cache = await open(c.req.path);
     const cachedResponse = await cache.match(c.req.raw);
 
