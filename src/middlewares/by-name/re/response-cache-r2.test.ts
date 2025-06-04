fd18d5bfb33a0f6bd71124cfe0effaf45adfd840
nyarla <nyarla@kalaclista.com>
2025-06-04 15:14:42 +0900

    refactor(middleware/response-cache-r2): the old code did not use `R2Cache` instance

--- src/middlewares/by-name/re/response-cache-r2.test.ts
+++ src/middlewares/by-name/re/response-cache-r2.test.ts
@@ -50,49 +50,27 @@ describe("response-cache-r2", () => {
     expect(bucket).toBeTruthy();
     expect(duration >= 0).toBeTruthy();
 
-    return {
-      async match(r: Request): Promise<Response | undefined> {
-        if (new URL(r.url).pathname === "/cached") {
-          return new Response("/cached", { status: 200 });
-        }
-
-        return undefined;
-      },
-
-      async put(r: Request, w: Response): Promise<void> {
-        expect(r).toBeInstanceOf(Request);
-        expect(r.bodyUsed).toBeFalsy();
-
-        expect(w).toBeInstanceOf(Response);
-        expect(w.bodyUsed).toBeFalsy();
-
-        return;
-      },
-
-      async delete(_: Request): Promise<boolean> {
-        return true;
-      },
-    };
+    return R2Cache(bucket, 1000);
   };
 
   const app = new Hono<{ Bindings: Bindings }>();
   app.get("*", middleware(open));
-  app.get("/ok", (c) => c.text("ok"));
-  app.get("/cached", (c) => c.text("ok"));
+  app.get("/ok", (c) => {
+    c.header("X-IS-TEST", "true");
+    c.header("Content-Type", "text/plain");
+    return c.body("ok");
+  });
   app.get("/notfound", (c) => c.notFound());
 
   it("the response returns 200 ok, it should be storing to cache.", async () => {
-    const ctx = createExecutionContext();
-    const res = await app.request("/ok", {}, env, ctx);
+    let res = await app.request("/ok", {}, env);
     expect(await res.text()).toBe("ok");
-    waitOnExecutionContext(ctx);
-  });
+    expect(res.headers.get("X-IS-TEST")).toBe("true");
 
-  it("the response data exists in cache, it should be returned as response data.", async () => {
-    const ctx = createExecutionContext();
-    const res = await app.request("/cached", {}, env, ctx);
-    expect(await res.text()).toBe("/cached");
-    waitOnExecutionContext(ctx);
+    res = await app.request("/ok", {}, env);
+    expect(await res.text()).toBe("ok");
+    expect(res.headers.get("Content-Type")).toBe("text/plain");
+    expect(res.headers.get("X-IS-TEST")).toBeFalsy();
   });
 
   it("the original response is not 200 ok, it should not be storing to cache", async () => {
