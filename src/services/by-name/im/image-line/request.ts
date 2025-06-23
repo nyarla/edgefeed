8e2a42d78bb64351ee049d6111ad943025ae570a
nyarla <nyarla@kalaclista.com>
2025-06-23 11:29:58 +0900

    feat(image-line): add function to make `Request` object for flstudio-news

--- src/services/by-name/im/image-line/request.ts
+++ src/services/by-name/im/image-line/request.ts
@@ -1,1 +1,17 @@
 export const FLStudioNewsUrl = "https://www.image-line.com/fl-studio-news";
+
+const defaultDummyUserAgent =
+  "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0";
+
+export const makeFLStudioNewRequest = (userAgent?: string): Request => {
+  const dummaryUserAgent = userAgent ?? defaultDummyUserAgent;
+  const request = new Request(FLStudioNewsUrl, {
+    method: "GET",
+    redirect: "follow",
+    headers: {
+      "User-Agent": dummaryUserAgent,
+    },
+  });
+
+  return request;
+};
