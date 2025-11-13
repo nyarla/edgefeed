de5f26c78f58eeb94341cbb51421803a358e1f74
nyarla <nyarla@kalaclista.com>
2025-11-13 19:44:41 +0900

    feat(services/melonbooks): add make functions of `Request` for ranking and new items pages

--- src/services/by-name/me/melonbooks/request.test.ts
+++ src/services/by-name/me/melonbooks/request.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, it } from "vitest";
-import { makeCirclePageRequest } from "./request";
+import {
+  makeCirclePageRequest,
+  makeNewItemsPageRequest,
+  makeRankingPageRequest,
+} from "./request";
 
 describe("melonbooks/request", () => {
   it("makeCirclePageRequest", () => {
@@ -20,5 +24,56 @@ describe("melonbooks/request", () => {
       "https://www.melonbooks.co.jp/circle/index.php?circle_id=2",
     );
     expect(r2.headers?.get("User-Agent")).toBe(ua);
+  });
+
+  it("makeRankingPageRequest", () => {
+    const ua = "test/0.0";
+    const r1 = makeRankingPageRequest(1, 2);
+
+    expect(r1).instanceof(Request);
+    expect(r1.url).toBe(
+      "https://www.melonbooks.co.jp/ranking/index.php?period=1&type=1&mode=category&category=2",
+    );
+
+    expect(r1.method).toBe("GET");
+    expect(r1.redirect).toBe("follow");
+    expect(r1.headers?.get("Cookie")).toBe("AUTH_ADULT=1");
+    expect(r1.headers?.get("User-Agent")).not.toBe(ua);
+
+    const r2 = makeRankingPageRequest(1, 2, ua);
+    expect(r2.url).toBe(r1.url);
+    expect(r2.headers?.get("User-Agent")).toBe(ua);
+  });
+
+  it("makeNewItemsPageRequest", () => {
+    const ua = "test/0.0";
+    const r1 = makeNewItemsPageRequest("new", 1);
+
+    expect(r1).instanceOf(Request);
+    expect(r1.redirect).toBe("follow");
+    expect(r1.headers?.get("Cookie")).toBe("AUTH_ADULT=1");
+    expect(r1.headers?.get("User-Agent")).not.toBe(ua);
+
+    expect(r1.url).toBe(
+      "https://www.melonbooks.co.jp/new/arrival.php?sort_type=0&category=1",
+    );
+
+    const r2 = makeNewItemsPageRequest("new", 1, ua);
+    expect(r2.url).toBe(r1.url);
+    expect(r2.headers?.get("User-Agent")).toBe(ua);
+
+    const r3 = makeNewItemsPageRequest("reserve", 2);
+    expect(r3).instanceOf(Request);
+    expect(r3.redirect).toBe("follow");
+    expect(r3.headers?.get("Cookie")).toBe("AUTH_ADULT=1");
+    expect(r3.headers?.get("User-Agent")).not.toBe(ua);
+
+    expect(r3.url).toBe(
+      "https://www.melonbooks.co.jp/reserve/reserve.php?sort_type=0&category=2",
+    );
+
+    const r4 = makeNewItemsPageRequest("reserve", 2, ua);
+    expect(r4.url).toBe(r3.url);
+    expect(r4.headers?.get("User-Agent")).toBe(ua);
   });
 });
--- src/services/by-name/me/melonbooks/request.ts
+++ src/services/by-name/me/melonbooks/request.ts
@@ -24,3 +24,64 @@ export const makeCirclePageRequest = (
 
   return request;
 };
+
+/**
+ * Make a `Request` object for fetching from melonbook's ranking page.
+ *
+ * @param {number} kind - the type of items on ranking page.
+ * @param {number} category - the category id on melonbooks shopping items.
+ * @param {string?} userAgent - the user-agent string for request.
+ * @returns {Request} - the `Request` object for fethcing from melonbooks's ranking page.
+ */
+export const makeRankingPageRequest = (
+  kind: number,
+  category: number,
+  userAgent?: string,
+): Request => {
+  const href = `https://www.melonbooks.co.jp/ranking/index.php?period=1&type=${kind}&mode=category&category=${category}`;
+  const dummyUserAgent = userAgent || defaultDummyUserAgent;
+  const request = new Request(href, {
+    method: "GET",
+    redirect: "follow",
+    headers: {
+      "User-Agent": dummyUserAgent,
+      Cookie: "AUTH_ADULT=1",
+    },
+  });
+
+  return request;
+};
+
+/**
+ * The kind of new items on melonbooks shopping service.
+ *
+ * The acceptable values of this type are `reserve` or `new`.
+ */
+export type Kind = "reserve" | "new";
+
+/**
+ * Make a `Request` object for fetching from melonbooks's new items pages.
+ *
+ * @param {Kind} kind - the kind of new items.
+ * @param {number} category - the category id on melonbooks shoppoing service.
+ * @param {string?} userAgent - the user-agent string of fetching request.
+ * @returns {Request} - the `Request` object for fetching new items page from melonbooks.
+ */
+export const makeNewItemsPageRequest = (
+  kind: Kind,
+  category: number,
+  userAgent?: string,
+): Request => {
+  const href = `https://www.melonbooks.co.jp/${kind === "new" ? "new/arrival" : `${kind}/${kind}`}.php?sort_type=0&category=${category}`;
+  const dummyUserAgent = userAgent || defaultDummyUserAgent;
+  const request = new Request(href, {
+    method: "GET",
+    redirect: "follow",
+    headers: {
+      "User-Agent": dummyUserAgent,
+      Cookie: "AUTH_ADULT=1",
+    },
+  });
+
+  return request;
+};
