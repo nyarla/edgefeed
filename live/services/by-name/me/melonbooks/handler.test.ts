60d03a68e481578bb2a13f153a6aa9923c15b74a
nyarla <nyarla@kalaclista.com>
2025-11-14 10:43:46 +0900

    feat(melonbooks): add `rankingPageToJSONFeed` and `newItemsPageToJSONFeed` handlers

--- live/services/by-name/me/melonbooks/handler.test.ts
+++ live/services/by-name/me/melonbooks/handler.test.ts
@@ -1,27 +1,103 @@
 import { Hono } from "hono";
 import { describe, expect, it } from "vitest";
 
-import { circlePageToJSONFeed } from "@/services/by-name/me/melonbooks/handlers";
+import {
+  circlePageToJSONFeed,
+  newItemsPageToJSONFeed,
+  rankingPageToJSONFeed,
+} from "@/services/by-name/me/melonbooks/handlers";
 
 const app = new Hono();
-app.get("/:id", circlePageToJSONFeed("https://example.com/"));
 
-describe("melonbooks/handlers", () => {
-  it("invalid url", async () => {
-    const res = await app.request("/foo", {}, {});
+app.get("/circle/:id", circlePageToJSONFeed("https://example.com/"));
+app.get("/new/:category/:kind", newItemsPageToJSONFeed("https://example.com/"));
+app.get(
+  "/ranking/:category/:type",
+  rankingPageToJSONFeed("https://example.com/"),
+);
 
-    expect(res.ok).not.toBeTruthy();
-    expect(res.status).toBe(400);
-  });
-
-  // This test failed on local environment.
-  // At the moment, melonbooks uses weak certificate to their website,
-  // it trigger to SSL error on local test
+describe("circlePageToJSONFeed", () => {
   it("live request", async () => {
-    const res = await app.request("/3753", {}, {});
+    const res = await app.request("/circle/3753", {}, {});
 
     expect(res.ok).toBeTruthy();
     expect(res.status).toBe(200);
-    expect(await res.json()).toBeTruthy();
+    expect(await res?.json()).toBeTruthy();
+  });
+
+  it("invalid url", async () => {
+    const res = await app.request("/circle/invalid", {}, {});
+
+    expect(res.ok).not.toBeTruthy();
+    expect(res.status).toBe(400);
+    expect(await res?.text()).toBe("circle id should be the number");
+  });
+});
+
+describe("newItemsPageToJSONFeed", () => {
+  it("live test: reserve", async () => {
+    const res = await app.request("/new/9/reserve");
+
+    expect(res.ok).toBeTruthy();
+    expect(res.status).toBe(200);
+    expect(await res?.json()).toBeTruthy();
+  });
+
+  it("live test: new", async () => {
+    const res = await app.request("/new/9/new");
+
+    expect(res.ok).toBeTruthy();
+    expect(res.status).toBe(200);
+    expect(await res?.json()).toBeTruthy();
+  });
+
+  it("invalid url: category", async () => {
+    const res1 = await app.request("/new/unknown/new", {}, {});
+
+    expect(res1.ok).toBeFalsy();
+    expect(res1.status).toBe(400);
+    expect(await res1?.text()).toBe("The category id should be a number");
+
+    const res2 = await app.request("/new/unknown/reserve", {}, {});
+
+    expect(res2.ok).toBeFalsy();
+    expect(res2.status).toBe(400);
+    expect(await res2?.text()).toBe("The category id should be a number");
+  });
+
+  it("invalid url: kind", async () => {
+    const res = await app.request("/new/9/unknown", {}, {});
+
+    expect(res.ok).toBeFalsy();
+    expect(res.status).toBe(400);
+    expect(await res?.text()).toBe(
+      "The 'kind' of page should be a 'reserve' or 'new', but this value is: unknown",
+    );
+  });
+});
+
+describe("rankingPageToJSONFeed", () => {
+  it("live test", async () => {
+    const res = await app.request("/ranking/9/2", {}, {});
+
+    expect(res.ok).toBeTruthy();
+    expect(res.status).toBe(200);
+    expect(await res?.json()).toBeTruthy();
+  });
+
+  it("invalid url: category", async () => {
+    const res = await app.request("/ranking/unknown/2", {}, {});
+
+    expect(res.ok).toBeFalsy();
+    expect(res.status).toBe(400);
+    expect(await res?.text()).toBe("The category id should be a number");
+  });
+
+  it("invalid url: type", async () => {
+    const res = await app.request("/ranking/9/unknown", {}, {});
+
+    expect(res.ok).toBeFalsy();
+    expect(res.status).toBe(400);
+    expect(await res?.text()).toBe("The type id should be a number");
   });
 });
--- src/services/by-name/me/melonbooks/handlers.ts
+++ src/services/by-name/me/melonbooks/handlers.ts
@@ -2,7 +2,11 @@ import type { Context, Handler } from "hono";
 import { HTTPException } from "hono/http-exception";
 
 import { transformToJSONFeed } from "./parse";
-import { makeCirclePageRequest } from "./request";
+import {
+  makeCirclePageRequest,
+  makeNewItemsPageRequest,
+  makeRankingPageRequest,
+} from "./request";
 
 /**
  * Make `Hono` handler for transform melonbooks's circle page to JSON Feed.
@@ -28,6 +32,98 @@ export const circlePageToJSONFeed =
     if (!response?.ok) {
       throw new HTTPException(500, {
         message: `failed to fetch melonbooks circle page: ${await response.text()}`,
+      });
+    }
+
+    return c.body(await transformToJSONFeed(response, baseUrl), 200, {
+      "Content-Type": "application/feed+json; charset=utf-8",
+    });
+  };
+
+/**
+ * Make the `Hono` handler for transform the ranking page to jsonfeed.
+ *
+ * @param {string} baseUrl - the base URL of JSON Feed.
+ * @param {string?} userAgent - the User-Agent string for fetching remote content.
+ * @returns {Handler} - the Hono handler.
+ */
+export const rankingPageToJSONFeed =
+  (baseUrl: string, userAgent?: string): Handler =>
+  /* oxlint-disable */
+  async (c: Context) => {
+    const category = c.req.param("category");
+    if (category?.match(/[^0-9]/)) {
+      throw new HTTPException(400, {
+        message: "The category id should be a number",
+      });
+    }
+
+    const typ = c.req.param("type");
+    if (typ?.match(/[^0-9]/)) {
+      throw new HTTPException(400, {
+        message: "The type id should be a number",
+      });
+    }
+
+    const request = makeRankingPageRequest(
+      Number.parseInt(typ, 10),
+      Number.parseInt(category, 10),
+      userAgent,
+    );
+    const response = await fetch(request);
+
+    if (!response?.ok) {
+      throw new HTTPException(500, {
+        message: `failed to fetch the melonbooks ranking page: ${await response.text()}`,
+      });
+    }
+
+    return c.body(await transformToJSONFeed(response, baseUrl), 200, {
+      "Content-Type": "application/feed+json; charset=utf-8",
+    });
+  };
+
+/**
+ * Make the `Hono` handler for transform new items pages to jsonfeed.
+ *
+ * @param {string} baseUrl - the base URL of jsonfeed.
+ * @param {string?} userAgent - the User-Agent string for fetch remote content.
+ * @returns {Handler} - the Hono handler.
+ **/
+export const newItemsPageToJSONFeed =
+  (baseUrl: string, userAgent?: string): Handler =>
+  /* oxlint-disable */
+  async (c: Context) => {
+    const category = c.req.param("category");
+    if (category?.match(/[^0-9]/)) {
+      throw new HTTPException(400, {
+        message: "The category id should be a number",
+      });
+    }
+
+    const kind = c.req.param("kind");
+    switch (kind) {
+      case "reserve":
+      case "new":
+        // nothing to do.
+        break;
+
+      default:
+        throw new HTTPException(400, {
+          message: `The 'kind' of page should be a 'reserve' or 'new', but this value is: ${kind}`,
+        });
+    }
+
+    const request = makeNewItemsPageRequest(
+      kind,
+      Number.parseInt(category, 10),
+      userAgent,
+    );
+    const response = await fetch(request);
+
+    if (!response?.ok) {
+      throw new HTTPException(500, {
+        message: `failed to fetch the melonbooks ranking page: ${await response.text()}`,
       });
     }
 
