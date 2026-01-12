82dd3ecb30bcc38f8eba108d36218766fac2e35b
nyarla <nyarla@kalaclista.com>
2026-01-12 13:38:25 +0900

    refactor(flake): use `sec` as credentials manager for development

--- flake.nix
+++ flake.nix
@@ -24,6 +24,15 @@
 
           shellHook = with pkgs; ''
             export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+
+            sec-exec() {
+              # `sec` can take from https://github.com/nyarla/sec
+              env -i $(SEC_ENV=development sec env | grep -P "EDGEFEED_[A-Z]+=" ; echo PATH=$PATH ; echo SSL_CERT_FILE=$SSL_CERT_FILE) "''${@:-}"
+            }
+
+            wrangler() {
+              sec-exec env CLOUDFLARE_INCLUDE_PROCESS_ENV=true bun run wrangler "''${@:-}"
+            }
           '';
         };
       }
