cae3fef160490e9e24a3d5793424bd9b86930af0
nyarla <nyarla@kalaclista.com>
2026-04-06 09:20:38 +0900

    chore(lib/const): update Firefox UA, and add Google Chrome UA

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,5 +1,11 @@
 /**
- * The UserAgent string from Firefox linux 141.0
+ * The UserAgent string from Firefox linux 149.0
  */
 export const FirefoxForLinuxUserAgent =
-  "Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0";
+  "Mozilla/5.0 (X11; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0";
+
+/**
+ * The UserAgent stirng from Google Chrome for Linux 146
+ */
+export const GoogleChromeLinuxUserAgent =
+  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36";
