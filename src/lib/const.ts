3323166931a77057ed216807d538ba56c2ebe523
nyarla <nyarla@kalaclista.com>
2026-04-06 09:24:48 +0900

    chore(lib/const): update variable name and documentation commments

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,5 +1,5 @@
 /**
- * The UserAgent string from Firefox linux 149.0
+ * The UserAgent string from Firefox for Linux 149.0
  */
 export const FirefoxForLinuxUserAgent =
   "Mozilla/5.0 (X11; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0";
@@ -7,5 +7,5 @@ export const FirefoxForLinuxUserAgent =
 /**
  * The UserAgent stirng from Google Chrome for Linux 146
  */
-export const GoogleChromeLinuxUserAgent =
+export const GoogleChromeForLinuxUserAgent =
   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36";
