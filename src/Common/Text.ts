934642b5eb2a7fc4055bce23f0a2a99e4ced44af
nyarla <nyarla@kalaclista.com>
2026-05-05 11:12:15 +0900

    fix(common): fix to oxlint error

--- src/Common/Text.ts
+++ src/Common/Text.ts
@@ -6,7 +6,7 @@
  *
  * Returns an empty string if the input is null or undefined.
  *
- * @param src? - The inputs string to normalize.
+ * @param src - The inputs string to normalize.
  * @returns The normalized string.
  **/
 export const normalizeText = (src?: string | null): string =>
